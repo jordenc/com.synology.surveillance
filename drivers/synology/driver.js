184dd0adcdc5448e8a610fc96b58e6b9de1889f0
Jorden Chamid <jchamid@totallyhosted.nl>
2016-04-22 15:02:18 +0200

    replaced NodeJS module with newer version, adding devices now works

--- drivers/synology/driver.js
+++ drivers/synology/driver.js
@@ -1,24 +1,101 @@
 "use strict";
 
 var http = require('http');
-var Synology = require('node-synology-surveillance');
+var Synology = require('synology');
+var tempdevices;
+var devices = [];
 
 module.exports.pair = function (socket) {
+	
+	function eachRecursive(obj)
+{
+    for (var k in obj)
+    {
+        if (typeof obj[k] == "object" && obj[k] !== null)
+            eachRecursive(obj[k]);
+        else
+            // do something... 
+            Homey.log (k + ' => ' + obj[k]);
+    }
+}
+
 	// socket is a direct channel to the front-end
 
 	// this method is run when Homey.emit('list_devices') is run on the front-end
 	// which happens when you use the template `list_devices`
 	socket.on('list_devices', function (data, callback) {
 
-		Homey.log("Synology Surveillance Station app - list_devices tempIP is " + tempIP);
+		Homey.log("Synology Surveillance Station app - list_devices called" );
 		
+		/*
+			for (var key in tempdevices) {
+				
+				if (tempdevices.hasOwnProperty(key)) {
+					Homey.log(key + " -> " + tempdevices[key]);
+	  			}
+	  			
+			}
+		*/
+		
+		//camIP
+		//camName
+		//camIdOnRecServer
+		//camModel
+		
+		for (var key in tempdevices) {
+			
+			var device = tempdevices[key];
+			
+			Homey.log ('device=' + device["name"]);
+			Homey.log ('ip=' + device["host"]);
+			Homey.log ('model=' + device["model"]);
+			
+			devices.push(
+				{
+					data: {
+						id			: device["host"],
+						ipaddress 	: device["host"],
+						model		: device["model"]
+					},
+					name: device["name"]
+				}
+			);
+				
+			//Homey.log ('key ' + key + ' = ' + JSON.stringify(device));
+			/*
+			for (var key in device) {
+				
+				if (device.hasOwnProperty(key)) {
+					Homey.log(key + " -> " + device[key]);
+	  			}
+	  			
+	  			devices.push(
+					[{
+						data: {
+							id			: device[key]["id"],
+							ipaddress 	: device[key]["host"],
+						},
+						name: device[key]["name"]
+					}]	
+				);
+	  			
+			}
+			*/
+			//Homey.log ('Adding ' + tempdevices[key]["camName"]);
+		
+
+			
+		}
+		//eachRecursive (tempdevices);
+		/*
 		var devices = [{
 			data: {
 				id			: tempIP,
 				ipaddress 	: tempIP
 			}
 		}];
-
+		*/
+		Homey.log ('callback with ' + JSON.stringify(devices));
 		callback (null, devices);
 
 	});
@@ -39,13 +116,21 @@ module.exports.pair = function (socket) {
 		    password: password
 		});
 		
-		syno.surveillance.info.get_info(function(err, data){
-		    if (err) throw err;
-		    Homey.log(data);
+		syno.query('/webapi/entry.cgi', {
+			api    : 'SYNO.SurveillanceStation.Camera',
+			version: 1,
+			method : 'List',
+			query  : 'ALL'
+		}, function(err, data) {
+			if (err) return console.error(err);
+			
+			//Homey.log(data);
+//			tempdevices = JSON.parse(data);
+			tempdevices = data.data.cameras;
+			
+			socket.emit ('continue', null);
 		});
-
-
-		socket.emit ('continue', null);
+		
 
 	});
 
